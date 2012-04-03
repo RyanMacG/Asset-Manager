73ead52fcb8978c4fb60066e183e26c67d43a1db
Ryan MacGillivray <rmacg89@gmail.com>
2012-04-03 21:23:24 +0100

    made routes better

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 AssetManager::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+   root to: 'static_pages#home'
+
+   get "static_pages/home"
+   get "static_pages/help"
+   get "static_pages/about"
+  
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,36 +4,36 @@ describe "Static Pages" do
 	
 	describe "Home page" do
 		it "should have the content 'Asset Manager'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Asset Manager')
 		end
     
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Asset Manager | Home")
     end
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
     
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "Asset Manager | Help")
     end
 	end
 
 	describe "About page" do
 		it "should have the content 'About the app" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About')
 		end
     
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "Asset Manager | About")
     end
 	end
