fcdb63106a2dbd1d95a94f270d4f9be1d6b37d79
Ryan MacGillivray <rmacg89@gmail.com>
2012-06-05 07:26:15 +0100

    basic prawn skeleton

--- app/pdfs/asset_pdf.rb
+++ app/pdfs/asset_pdf.rb
@@ -15,29 +15,43 @@
 #  created_at        :datetime        not null
 #  updated_at        :datetime        not null
 #
-class AssetPdf <  Prawn::Document
+class AssetPdf
   def initialize(asset)
-    super()
     @asset  = asset
+    @pdf = Prawn::Document.new
     asset_intro
     blank_line
     asset_user
     purchase_deets
+    blank_line
+    render
   end
   
   def asset_intro
-    text "Asset - JTC#{@asset.id} | #{@asset.asset_description}", size: 18, style: :bold
+    @pdf.text "Asset - JTC#{@asset.id} | #{@asset.asset_description} #{@asset.asset_type}", size: 18, style: :bold
   end
   
   def asset_user
-    text "This asset is assigned to #{@asset.user.name} and was last changed at #{@asset.updated_at}"
+    @pdf.text "This asset is assigned to #{@asset.user.name} and was last changed at #{@asset.updated_at}"
   end
   
   def purchase_deets
-    text "It was purchased for GBP(#{@asset.cost}) on #{@asset.date_purchased}"
+    @pdf.text "It was purchased for GBP(#{@asset.cost}) on #{@asset.date_purchased}"
+  end
+  
+  def comment
+    @pdf.text "The following issues/comments have been recorded #{@asset.comment}"
+  end
+  
+  def serial
+    @pdf.text "For reference the Serial No/IMEI is"
   end
   
   def blank_line
-    text " "
+    @pdf.text " "
+  end
+  
+  def render
+    @pdf.render
   end
 end
