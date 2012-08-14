9a724bc55b4b6aff9a2252ea449e956f98e87ca7
Ryan MacGillivray <rmacg89@gmail.com>
2012-08-14 13:29:34 +0100

    more capistrano recipes

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -1,4 +1,5 @@
 namespace :nginx do
+  desc "Install latest stable release of nginx"
   task :install do
     run "#{sudo} add-apt-repository ppa:nginx/stable"
     run "#{sudo} apt-get -y update"
@@ -7,6 +8,7 @@ namespace :nginx do
   after "deploy:install", "nginx:install"
   
   task :setup do
+    desc "Setup nginx config for the app"
     template "nginx_unicorn.erb", "/tmp/nginx_conf"
     run "#{sudo} mv /tmp/nginx_conf /ect/nginx/sites-enabled/#{application}"
     run "#{sudo} rm -f /etc/sites-enabled/default"
@@ -15,6 +17,7 @@ namespace :nginx do
   after "deploy:setup", "nginx:setup"
   
   %w[start stop restart].each do |command|
+    desc "#{command} nginx."
     task command do
       run "#{sudo} service nginx #{command}"
     end
