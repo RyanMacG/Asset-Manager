76f7247e5b0b25176e5a1eca7b9790438366ec2c
Ryan MacGillivray <rmacg89@gmail.com>
2012-08-20 11:03:56 +0100

    hopefully fix some issues with the capistrano recipes

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -9,7 +9,7 @@ namespace :unicorn do
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "unicorn.rb.erb", unicorn_config
-    template "unicorn_init.erb", "/tmp/unicorn_init"
+    template "unicorn_init.erb", "tmp/unicorn_init"
     run "chmod +x /tmp/unicorn_init"
     run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
     run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
