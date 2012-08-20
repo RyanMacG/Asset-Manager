80d106af6d97025ce7ea5106d4f853bf201f349e
Ryan MacGillivray <rmacg89@gmail.com>
2012-08-20 12:18:22 +0100

    changed some recipes to reduce downtime, added in maintenance page, fixed some issues with existing recipes

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -1,24 +1,24 @@
 namespace :nginx do
   desc "Install latest stable release of nginx"
-  task :install do
+  task :install, roles: :web do
     run "#{sudo} add-apt-repository ppa:nginx/stable"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install nginx"
   end
   after "deploy:install", "nginx:install"
-  
-  task :setup do
-    desc "Setup nginx config for the app"
+
+  desc "Setup nginx configuration for this application"
+  task :setup, roles: :web do
     template "nginx_unicorn.erb", "/tmp/nginx_conf"
-    run "#{sudo} mv /tmp/nginx_conf /ect/nginx/sites-enabled/#{application}"
-    run "#{sudo} rm -f /etc/sites-enabled/default"
-    run "#{sudo} service nginx restart"
+    run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
+    run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
+    restart
   end
   after "deploy:setup", "nginx:setup"
   
   %w[start stop restart].each do |command|
-    desc "#{command} nginx."
-    task command do
+    desc "#{command} nginx"
+    task command, roles: :web do
       run "#{sudo} service nginx #{command}"
     end
   end
