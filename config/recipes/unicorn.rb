4bc8d494afefa1b5bfd13d62cc5618d5df4a838f
Ryan MacGillivray <rmacg89@gmail.com>
2012-08-20 11:03:56 +0100

    hopefully fix some issues with the capistrano recipes

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -1,11 +1,11 @@
 set_default(:unicorn_user) { user }
 set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
-set_default(:unicorn_log){ "#{shared_path}/log/unicorn.log" }
+set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers, 2)
 
 namespace :unicorn do
-  desc "Setup unicorn initialiser and app configuration"
+  desc "Setup Unicorn initializer and app configuration"
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "unicorn.rb.erb", unicorn_config
@@ -15,7 +15,7 @@ namespace :unicorn do
     run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
   end
   after "deploy:setup", "unicorn:setup"
-  
+
   %w[start stop restart].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
