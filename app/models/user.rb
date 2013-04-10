2be3756f810299912a3f2ff85e780f0332bace1c
Ryan MacGillivray <rmacg89@gmail.com>
2013-04-10 16:13:21 +0100

    added ability to import existing users and assets from the old system

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,46 +13,65 @@
 #
 
 class User < ActiveRecord::Base
-   attr_accessible :name, :email, :password, :password_confirmation, :admin
-   has_secure_password
-   has_many :assets
-   
-   before_save { |user| user.email = email.downcase }
-   before_save :create_remember_token
-   after_save :toggle_admin
-   
-   validates :name,  presence: true, length: { maximum: 80 }
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-             uniqueness: { case_sensitive: false }
-             
-   validates_presence_of :password, length: { minimum: 6 }, on: :create
-   validates_presence_of :password_confirmation, presence: true, on: :create
-   
-   def feed
-     Asset.where("user_id = ?", id)
-   end
-   
-   def send_password_reset
-     create_reset_token
-     self.password_reset_sent_at = Time.zone.now
-     save!
-     UserMailer.password_reset(self).deliver
-   end
-   
-   private 
-   
-     def create_remember_token
-       self.remember_token = SecureRandom.urlsafe_base64
-     end
-     
-     def create_reset_token
-       self.password_reset_token = SecureRandom.urlsafe_base64
-     end
+  attr_accessible :name, :email, :password, :password_confirmation, :admin
+  has_secure_password
+  has_many :assets
 
-     def toggle_admin
-      if self.admin == true
-        self.toggle!(:admin)
-      end
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+  after_save :toggle_admin
+
+  validates :name,  presence: true, length: { maximum: 80 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: { case_sensitive: false }
+
+  validates_presence_of :password, length: { minimum: 6 }, on: :create
+  validates_presence_of :password_confirmation, presence: true, on: :create
+
+  def feed
+    Asset.where("user_id = ?", id)
+  end
+
+  def send_password_reset
+    create_reset_token
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
+
+  def self.import(file)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      user = find_by_id(row["id"]) || new
+      user.attributes = row.to_hash.slice(*accessible_attributes)
+      user.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
+    when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
+    def create_reset_token
+      self.password_reset_token = SecureRandom.urlsafe_base64
+    end
+
+    def toggle_admin
+     if self.admin == true
+       self.toggle!(:admin)
+     end
     end
 end
