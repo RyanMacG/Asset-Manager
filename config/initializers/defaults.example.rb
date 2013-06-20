afe23a3330bd48794e084a9ec142da549ef24017
Ryan MacGillivray <rmacg89@gmail.com>
2013-06-20 14:00:55 +0100

    made parts of the help page environment variable

--- config/initializers/defaults.example.rb
+++ config/initializers/defaults.example.rb
@@ -4,3 +4,5 @@ ENV['asset_pre'] = 'IDENT' #whatever your company uses as a unique identifier be
 ENV['currency'] = 'GBP' #your local currency
 ENV['comp_address_text'] = 'company name' #name of company/something else
 ENV['comp_address'] = 'http://compintranet' #whatever company address is relevant
+ENV['comp_email'] = 'mailto:example@example.com?subject=Issue%20with%20asset%20manager' #the email address of whoever is support this
+ENV['support_name'] = 'John Bloggs' # the name of whoever/what department is responsible
