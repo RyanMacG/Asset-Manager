a104c262c1df462abe055b345e1f4e862b6656b9
Ryan MacGillivray <rmacg89@gmail.com>
2013-06-22 23:22:55 +0100

    trailing whitespace removal from dev generated files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,7 +23,7 @@ AssetManager::Application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
+  config.action_controller.allow_forgery_protection = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
@@ -35,7 +35,7 @@ AssetManager::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
+
   #Speed up our tests by lowering BCrypt's cost function
   require 'bcrypt'
   silence_warnings do
