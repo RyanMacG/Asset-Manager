41c352d1de378aed72678f4598317cfd4af9bf90
Ryan MacGillivray <rmacg89@gmail.com>
2013-06-25 12:36:07 +0100

    reconfigured some recipes due to issues, added additional stylesheets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,12 +46,12 @@ AssetManager::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application-all.css application-ie.css print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: "heroku.com" }
+  config.action_mailer.default_url_options = { host: "localhost" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
