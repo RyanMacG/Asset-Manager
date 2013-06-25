d9e971e7536ff06ef93d3bd9a76555da829f56e8
Ryan MacGillivray <rmacg89@gmail.com>
2013-06-25 08:51:13 +0100

    some capistrano fixes

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -23,7 +23,7 @@ namespace :postgresql do
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
-    cp "config/initializers/defaults.example.rb", "#{shared_path}/config/initializers/defaults.rb"
+    template "config/initializers/defaults.example.rb", "#{shared_path}/config/initializers/defaults.rb"
   end
   after "deploy:setup", "postgresql:setup"
 
