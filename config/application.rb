63ee481913e4e124e57d6c197b6dbb419845be23
Ryan MacGillivray <rmacg89@gmail.com>
2013-10-23 22:47:16 +0100

    only require roo now

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-
-require 'iconv'
-require 'csv'
+require 'roo'
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
