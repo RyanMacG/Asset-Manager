87d5102a57f416536a9954ff8f5da7d6ca9bee25
Ryan MacGillivray <rmacg89@gmail.com>
2014-05-17 12:47:53 +0100

    update user params to accept admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
 
     def correct_user
